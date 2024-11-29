de1595befa17a81f4ea7a391bf708d173aaa0187
gregnordin <nordin@fmailbox.com>
2024-11-29 15:13:50 -0700

    Working example ltk write text to file, show terminal and use print()

---
+++ 2024-11-26/try_write_file_02_ltk_v0.2.9/main.py
@@ -0,0 +1,37 @@
+import ltk
+
+
+@ltk.callback
+def save_to_file(event):
+    print(ltk.find("#content").val())
+    ltk.File().download("example.txt", ltk.find("#content").val())
+
+
+def create():
+    margin_left = ("margin-left", 10)
+    # Example string
+    my_string = """This is an example string
+with multiple lines
+of text content."""
+
+    return ltk.VBox(
+        ltk.Heading1("Save text to a file").css(*margin_left),
+        ltk.Label("Text to save to file:").css(*margin_left),
+        ltk.TextArea(
+            my_string,
+            {
+                "height": 200,
+                "margin-left": margin_left[1] + 20,
+                "margin-right": 20,
+                "padding": 8,
+                "background-color": "lightyellow",
+                "border": "1px solid gray",
+            },
+        ).attr("id", "content"),
+        ltk.Button("Save text to file", save_to_file)
+        .css("margin-top", 5)
+        .css(*margin_left),
+    )
+
+print("Hello world")
+create().appendTo(ltk.find("body"))
