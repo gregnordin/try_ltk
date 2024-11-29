840680851bc8bfae3341ba4c871a165ecbf52d11
gregnordin <nordin@fmailbox.com>
2024-11-28 17:55:34 -0700

    Try ltk.Code for showing file contents

--- 2024-11-26/try_read_file_04_ltk_v0.2.2.8/main.py
+++ 2024-11-26/try_read_file_04_ltk_v0.2.2.8/main.py
@@ -1,26 +1,23 @@
 import ltk
 
 
-def feedback(text):
-    ltk.find("#feedback").html(text).css("text-align", "left")
-
-
 @ltk.callback
 def loaded_file(file, content):
-    feedback(f"File name: {file.name}")
-    output_div = ltk.find("#content")
-    output_div.html(content)
+    ltk.find("#content").html(content)
 
 
 def create():
     return ltk.VBox(
         ltk.Heading1("Can we read a file?"),
         ltk.File(loaded_file),
-        ltk.Text("File name goes here").attr("id", "feedback"),
         ltk.Label("File contents:"),
-        ltk.TextArea(
-            "File contents go here",
-            {"height": 120, "width": "90%", "margin-left": 20, "fontsize": 10},
+        ltk.Code(
+            "Plaintext",
+            "File contents will be shown here after selecting file.",
+            style={
+                "width": "90%",
+                "margin-left": 20,
+            },
         ).attr("id", "content"),
     )
 
